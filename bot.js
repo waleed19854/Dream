fa068e7db38dfac8986637a728f5e456eb3a2e05
waleed19854 <46799814+waleed19854@users.noreply.github.com>
2019-01-30 01:20:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`ShaDoW|Walid`,"http://twitch.tv/KOSOM EL discord")
 client.user.setStatus("dnd")
 });
 
